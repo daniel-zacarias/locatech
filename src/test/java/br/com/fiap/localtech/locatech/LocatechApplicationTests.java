12117dbf0a98b22ea64e24a84a224c0367e47fac
daniel-zacarias <daniel.zacarias@bantec.com.br>
2025-04-13 13:29:05 -0300

    Add pom.xml

---
+++ src/test/java/br/com/fiap/localtech/locatech/LocatechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.localtech.locatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
